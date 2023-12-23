b0a8ddacc1996eb24df7749d48e758c63306edd0
Samet Şahin <samet.sahin.0122@hotmail.com>
2023-12-23 22:40:49 +0300

    integrate charts and conditional rendering

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,28 +1,100 @@
-import React from "react";
-import { Card, CardHeader, Text } from "@ui5/webcomponents-react";
+import React, { useState } from "react";
+import { Card, CardHeader, Text, Icon } from "@ui5/webcomponents-react";
 import { spacing } from "@ui5/webcomponents-react-base";
+import { BarChart, LineChart } from "@ui5/webcomponents-react-charts";
+import lineChartIcon from '@ui5/webcomponents-icons/dist/line-chart.js';
+import barChartIcon from '@ui5/webcomponents-icons/dist/horizontal-bar-chart.js';
+
+const dataset = [
+  {
+    month: "January",
+    data: 65
+  },
+  {
+    month: "February",
+    data: 59
+  },
+  {
+    month: "March",
+    data: 80
+  },
+  {
+    month: "April",
+    data: 81
+  },
+  {
+    month: "May",
+    data: 56
+  },
+  {
+    month: "June",
+    data: 55
+  },
+  {
+    month: "July",
+    data: 40
+  }
+];
 
 export function MyApp() {
-  
+  const [toggleCharts, setToggleCharts] = useState('lineChart');
+  const [loading, setLoading] = useState(false);
+
+  const contentTitle = toggleCharts === 'lineChart' ? 'Line Chart' : 'Bar Chart';
+  const switchToChart = toggleCharts === 'lineChart' ? 'Bar Chart' : 'Line Chart';
+
   const handleHeaderClick = () => {
-    alert("Header clicked");
-  };
+    if (toggleCharts === "lineChart") {
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+        setToggleCharts("barChart");
+      }, 2000);
+    } else {
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+        setToggleCharts("lineChart");
+      }, 2000);
+    }
+};
 
   return (
     <div>
       <Card
         header={
           <CardHeader
-            titleText="Card"
+            titleText="Stock Prices"
+            subtitleText={`Click here to switch to ${switchToChart}`}
             interactive
             onClick={handleHeaderClick}
+            avatar={
+              <Icon
+                name={
+                  toggleCharts === 'lineChart' ? lineChartIcon : barChartIcon
+                }
+              />
+            }
           />
         }
         style={{ width: "300px" }}
       >
-        <Text style={spacing.sapUiContentPadding}>
-          This is the content area of the Card
-        </Text>
+        <Text style={spacing.sapUiContentPadding}> {contentTitle} </Text>
+        {toggleCharts === 'lineChart' ? (
+          <LineChart
+            dimensions={[{ accessor: "month" }]}
+            measures={[{ accessor: "data", label: "Stock Price" }]}
+            dataset={dataset}
+            loading={loading}
+          />
+        ) : (
+          <BarChart
+            dimensions={[{ accessor: "month" }]}
+            measures={[{ accessor: "data" }]}
+            dataset={dataset}
+            loading={loading}
+          />
+        )}
       </Card>
     </div>
   );
